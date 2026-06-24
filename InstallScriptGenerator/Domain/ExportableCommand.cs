aba5fe9aa6b780c2adf1fe5226eacbd8f6d05acd
oldmartijntje <oldmartijntje@gmail.com>
2026-06-24 15:20:32 +0200

    init exportableCommand.cs

--- InstallScriptGenerator/Domain/ExportableCommand.cs
+++ InstallScriptGenerator/Domain/ExportableCommand.cs
@@ -2,5 +2,24 @@ namespace InstallScriptGenerator.Domain;
 
 public class ExportableCommand
 {
+    private List<string> _values = [];
+
+    public ExportableCommand(List<string>? values)
+    {
+        if (values != null)
+        {
+            this._values = values;
+        }
+    }
+    
+    public List<string> Values
+    {
+        get { return this._values; }
+    }
+
+    public void AddValue(string value)
+    {
+        this._values.Add(value);
+    }
     
 }
