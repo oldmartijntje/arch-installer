5560663e45727c0f6c478ceedef438ab0c585a4b
oldmartijntje <oldmartijntje@gmail.com>
2026-06-24 16:49:09 +0200

    initiate commandhandler

--- InstallScriptGenerator/ApplicationLayers/HandlerLayer/CommandHandler.cs
+++ InstallScriptGenerator/ApplicationLayers/HandlerLayer/CommandHandler.cs
@@ -1,6 +1,16 @@
+using InstallScriptGenerator.DataShareObjects;
+
 namespace InstallScriptGenerator.ApplicationLayers.HandlerLayer;
 
 public class CommandHandler
 {
-    
+    public List<CommandValidationReport> ValidateCommands()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RunCommands()
+    {
+        throw new NotImplementedException();
+    }
 }
